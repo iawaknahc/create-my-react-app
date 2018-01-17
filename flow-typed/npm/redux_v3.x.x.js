b2b46d1620b2f322df73f46bc8941312d1b02b9b
Louis Chan <louischan@oursky.com>
2018-01-17 14:49:35 +0800

    Update redux type declaration to support redux-thunk natively

--- flow-typed/npm/redux_v3.x.x.js
+++ flow-typed/npm/redux_v3.x.x.js
@@ -1,6 +1,7 @@
 // flow-typed signature: ec7daead5cb4fec5ab25fedbedef29e8
 // flow-typed version: 2c04631d20/redux_v3.x.x/flow_>=v0.55.x
 
+
 declare module 'redux' {
 
   /*
@@ -11,15 +12,16 @@ declare module 'redux' {
 
   */
 
-  declare export type DispatchAPI<A> = (action: A) => A;
-  declare export type Dispatch<A: { type: $Subtype<string> }> = DispatchAPI<A>;
+  declare export type PlainDispatch<A: { +type: $Subtype<string> }> = (action: A) => A;
+  declare export type ThunkDispatch<S, A> = <T>(action: (dispatch: Dispatch<S, A>, getState: () => S) => T) => T;
+  declare export type Dispatch<S, A> = PlainDispatch<A> & ThunkDispatch<S, A>;
 
-  declare export type MiddlewareAPI<S, A, D = Dispatch<A>> = {
+  declare export type MiddlewareAPI<S, A, D = Dispatch<S, A>> = {
     dispatch: D;
     getState(): S;
   };
 
-  declare export type Store<S, A, D = Dispatch<A>> = {
+  declare export type Store<S, A, D = Dispatch<S, A>> = {
     // rewrite MiddlewareAPI members in order to get nicer error messages (intersections produce long messages)
     dispatch: D;
     getState(): S;
@@ -31,16 +33,16 @@ declare module 'redux' {
 
   declare export type CombinedReducer<S, A> = (state: $Shape<S> & {} | void, action: A) => S;
 
-  declare export type Middleware<S, A, D = Dispatch<A>> =
+  declare export type Middleware<S, A, D = Dispatch<S, A>> =
     (api: MiddlewareAPI<S, A, D>) =>
       (next: D) => D;
 
-  declare export type StoreCreator<S, A, D = Dispatch<A>> = {
+  declare export type StoreCreator<S, A, D = Dispatch<S, A>> = {
     (reducer: Reducer<S, A>, enhancer?: StoreEnhancer<S, A, D>): Store<S, A, D>;
     (reducer: Reducer<S, A>, preloadedState: S, enhancer?: StoreEnhancer<S, A, D>): Store<S, A, D>;
   };
 
-  declare export type StoreEnhancer<S, A, D = Dispatch<A>> = (next: StoreCreator<S, A, D>) => StoreCreator<S, A, D>;
+  declare export type StoreEnhancer<S, A, D = Dispatch<S, A>> = (next: StoreCreator<S, A, D>) => StoreCreator<S, A, D>;
 
   declare export function createStore<S, A, D>(reducer: Reducer<S, A>, enhancer?: StoreEnhancer<S, A, D>): Store<S, A, D>;
   declare export function createStore<S, A, D>(reducer: Reducer<S, A>, preloadedState: S, enhancer?: StoreEnhancer<S, A, D>): Store<S, A, D>;
@@ -50,8 +52,8 @@ declare module 'redux' {
   declare export type ActionCreator<A, B> = (...args: Array<B>) => A;
   declare export type ActionCreators<K, A> = { [key: K]: ActionCreator<A, any> };
 
-  declare export function bindActionCreators<A, C: ActionCreator<A, any>, D: DispatchAPI<A>>(actionCreator: C, dispatch: D): C;
-  declare export function bindActionCreators<A, K, C: ActionCreators<K, A>, D: DispatchAPI<A>>(actionCreators: C, dispatch: D): C;
+  declare export function bindActionCreators<A, C: ActionCreator<A, any>, D>(actionCreator: C, dispatch: D): C;
+  declare export function bindActionCreators<A, K, C: ActionCreators<K, A>, D>(actionCreators: C, dispatch: D): C;
 
   declare export function combineReducers<O: Object, A>(reducers: O): CombinedReducer<$ObjMap<O, <S>(r: Reducer<S, any>) => S>, A>;
 
